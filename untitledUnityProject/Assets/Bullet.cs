19a40e15d59405cdf61698719902544c603112b0
Nicholas King <47709782+KlausKingsman@users.noreply.github.com>
2023-10-11 15:22:43 -0600

    Update Bullet.cs

--- untitledUnityProject/Assets/Bullet.cs
+++ untitledUnityProject/Assets/Bullet.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float destroyDelay = 2.0f; // Set the time in seconds after which the bullet should be destroyed.
+
+    private void Start()
+    {
+        // Call the DestroyBullet method after the specified delay.
+        Invoke("DestroyBullet", destroyDelay);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBullet(); // Call this method on collision as well.
+    }
+
+    private void DestroyBullet()
+    {
+        // Destroy the bullet game object.
         Destroy(gameObject);
     }
 }
